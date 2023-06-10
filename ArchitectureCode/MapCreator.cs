51bc32e766df1f857ad6f0ba81f5faf0a6f4191c
niapus <136102608+niapus@users.noreply.github.com>
2023-06-10 11:03:44 +0500

    Add files via upload

---
+++ ArchitectureCode/MapCreator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Drawing;
+
+namespace Elementaria
+{
+    public static class MapCreator
+    {
+        private static readonly Dictionary<string, Func<ICreature>> factory = new Dictionary<string, Func<ICreature>>();
+
+        public static ICreature[,] CreateMap(string map, string splitter = "\r\n")
+        {
+            var rows = map.Split(new[] { splitter }, StringSplitOptions.RemoveEmptyEntries);
+            var result = new ICreature[rows[0].Length, rows.Length];
+            for (var x = 0; x < rows[0].Length; x++)
+                for (var y = 0; y < rows.Length; y++)
+                    result[x, y] = CreateCreatureBySymbol(rows[y][x]);
+            return result;
+        }
+
+        private static ICreature CreateCreatureByTypeName(string name)
+        {
+            if (!factory.ContainsKey(name))
+            {
+                var type = Assembly
+                    .GetExecutingAssembly()
+                    .GetTypes()
+                    .FirstOrDefault(z => z.Name == name);
+                if (type == null)
+                    throw new Exception($"Can't find type '{name}'");
+                factory[name] = () => (ICreature)Activator.CreateInstance(type);
+            }
+            return factory[name]();
+        }
+
+        private static ICreature CreateCreatureBySymbol(char c)
+        {
+            switch (c)
+            {
+                case 'P':
+                    return CreateCreatureByTypeName("Player");
+                case 'S':
+                    return CreateCreatureByTypeName("Stone");
+                case 'B':
+                    return CreateCreatureByTypeName("Box");
+                case 'C':
+                    return CreateCreatureByTypeName("Chest");
+                case ' ':
+                    return null;
+                default:
+                    throw new Exception($"wrong character for Create");
+            }
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elementaria
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Create_Map_Single_Value()
+        {
+            var result = MapCreator.CreateMap("S");
+            Assert.IsTrue(result[0, 0] is Stone);
+        }
+
+        [TestCase("S", true)]
+        [TestCase("C", true)]
+        [TestCase("P", true)]
+        [TestCase(" ", true)]
+        [TestCase("B", true)]
+
+        public void Create_Map_Find_Element(string value, bool result)
+        {
+            var element = MapCreator.CreateMap(value);
+            Assert.AreEqual((element[0, 0] is Stone || element[0, 0] is Box || element[0, 0] is Player
+                || element[0, 0] is Chest || element[0, 0] is null), result);
+        }
+
+
+    }
+
+}
